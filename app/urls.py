cd1b3c78b21ca35fddcd1760a01a47f89b41c893
Justin Jenkins <jtjenkins07@gmail.com>
2024-05-21 20:37:41 -0500

    implement sign-in, sign-out

--- app/urls.py
+++ app/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', home, name="home"),
     path('signup/', signup, name="signup"),
     path('signin/', signin, name="signin"),
+    path('signout/', signout, name="signout"),
 ]
--- app/views.py
+++ app/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.models import User, auth
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from .models import *
+
 # Create your views here.
 def home(request):
   return render(request, "index.html")
@@ -24,6 +26,7 @@ def signup(request):
         user.save()
 
         #Log user in and redirect to settings page.
+        ...
 
         #Create a profile object for the new user.
         user_model = User.objects.get(username=username)
@@ -38,7 +41,22 @@ def signup(request):
   
 def signin(request):
   if request.method == 'POST':
-    pass
-  else:
-    ...
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(request, username=username, password=password)
+    ########### TEMPORARY ############
+    # if user.is_authenticated:
+    #   print(user)
+    ##################################
+    if user is not None:
+      login(request, user)
+      return redirect('home')
+    else:
+      messages.info(request, "Input does not match user's information on file. Please try again.")
+      redirect('signin')
   return render(request, "signin.html")
+
+def signout(request):
+  logout(request)
+  messages.success(request, 'You have successfully signed out.')
+  return redirect('signin')
